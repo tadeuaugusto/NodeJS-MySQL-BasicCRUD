cbd5e5cf067ea58a4a5d1c9b90e687031b755fac
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2019-03-13 03:12:20 -0300

    Add New User (endpoint POST /usuarios)

--- index.js
+++ index.js
@@ -30,12 +30,25 @@ router.delete('/usuarios/:id', (req, res) => {
     execSQLQuery('DELETE FROM Usuarios WHERE ID=' + parseInt(req.params.id), res);
 });
 
+// add new user
+// test: curl -X POST -d "nome=tadeu&email=tadeu@test.com&curso=Engenharia&turno=Integral&ra=1234567&telefone=5225" http://localhost:3000/usuarios
+router.post('/usuarios', (req, res) => {
+    const nome = req.body.nome.substring(0, 150);
+    const email = req.body.email.substring(0, 150);
+    const curso = req.body.curso.substring(0, 150);
+    const turno = req.body.turno.substring(0, 50);
+    const ra = req.body.ra.substring(0, 50);
+    const telefone = req.body.telefone.substring(0, 50);
+    execSQLQuery(`INSERT INTO Usuarios(nome, email, curso, turno, ra, telefone) `+
+        `VALUES('${nome}', '${email}', '${curso}', '${turno}', '${ra}', '${telefone}')`, res);    
+})
+
 // starting server
 app.listen(port);
 console.log('API is working!!!');
 
 
-/****************************** 
+/******************************* 
  * Generic SQL Query execution */
 function execSQLQuery(sqlQuery, res) {
     const conn = mysql.createConnection({
