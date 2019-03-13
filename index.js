312b0b6468b0ab9af82c268d882ea3a9b28784c0
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2019-03-13 03:31:23 -0300

    Update user (PATCH)

--- index.js
+++ index.js
@@ -43,6 +43,26 @@ router.post('/usuarios', (req, res) => {
         `VALUES('${nome}', '${email}', '${curso}', '${turno}', '${ra}', '${telefone}')`, res);    
 })
 
+// update user but never modify its id
+// test: curl -X PATCH -d "nome=tadeu&email=tadeu@test.com&curso=Engenharia&turno=Integral&ra=1234567&telefone=5225" http://localhost:3000/usuarios/2
+router.patch('/usuarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const nome = req.body.nome.substring(0, 150);
+    const email = req.body.email.substring(0, 150);
+    const curso = req.body.curso.substring(0, 150);
+    const turno = req.body.turno.substring(0, 50);
+    const ra = req.body.ra.substring(0, 50);
+    const telefone = req.body.telefone.substring(0, 50);
+    execSQLQuery(`UPDATE Usuarios SET `+
+                `nome='${nome}', `+
+                `email='${email}', `+
+                `curso='${curso}', `+
+                `turno='${turno}', `+
+                `ra='${ra}', `+
+                `telefone='${telefone}' WHERE id=${id}`, res);
+});
+
+
 // starting server
 app.listen(port);
 console.log('API is working!!!');
